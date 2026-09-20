93ea002a938c1dc6fbce66165f669f0d30c12095
JAGDISH <js8724285@gmail.com>
2026-09-20 23:52:21 +0530

    feat: use Thunderforest Neighbourhood tile layer and update CSP

--- client/src/components/ListingMap.jsx
+++ client/src/components/ListingMap.jsx
@@ -16,7 +16,7 @@ L.Marker.prototype.options.icon = DefaultIcon;
 
 const THUNDERFOREST_API_KEY = import.meta.env.VITE_THUNDERFOREST_API_KEY;
 const tileUrl = THUNDERFOREST_API_KEY
-  ? `https://tile.thunderforest.com/{z}/{x}/{y}.png?apikey=${THUNDERFOREST_API_KEY}`
+  ? `https://api.thunderforest.com/neighbourhood/{z}/{x}/{y}.png?apikey=${THUNDERFOREST_API_KEY}`
   : null;
 
 export default function ListingMap({ lat, lng, title, zoom = 13 }) {
